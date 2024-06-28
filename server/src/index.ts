8ea1a0a8d3fbb4a2d393b68d6f5c4080ba486a10
homeniuk <homeniuk@i.ua>
2024-06-28 17:38:13 +0300

    base lib

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from 'express';
-import dotenv from "dotenv";
+import 'dotenv/config';
 import cors, { CorsOptions } from "cors";
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
-dotenv.config();
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT;
+const DB_URL = String(process.env.DB_URL);
+
+
 const corsOptions: CorsOptions = {
+  credentials: true,
   origin: "http://localhost:3000"
 };
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
 
@@ -17,9 +25,19 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
+/*app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+});*/
 
-//npm start
+const start = async () => {
+  try {
+      await mongoose.connect(DB_URL)
+      app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+  } catch (e) {
+      console.log(e);
+  }
+}
+
+start()
+//cd server
 //npm run dev
